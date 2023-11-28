36ff505aa96d1d4d7a219629cfddb8ffdf0b23e2
Katasravani <105917453+Katasravani@users.noreply.github.com>
2023-11-28 12:45:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-var isDate = function (input) {
-  //   write your code here
-};
+function isDate(input) {
+  if (input instanceof Date) {
+    return !isNaN(input.getTime());
+  }
 
-// Do not change the code below.
-const input = prompt("Enter Date.");
-alert(isDate(input));
+  if (typeof input === 'string') {
+    const date = new Date(input);
+    return !isNaN(date.getTime());
+  }
+
+  return false;
+}
+
+function handleInput() {
+  const userInput = prompt('Enter a date:');
+  if (userInput === null) {
+    alert('Invalid input. Please enter a valid date.');
+    return;
+  }
+
+  const result = isDate(userInput);
+  alert(`Is it a valid date? ${result}`);
+}
